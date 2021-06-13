29bbff193526567846b22a2be543ed56e4806f10
Daniil Popov <pde-rnd@yandex.ru>
2021-06-13 16:40:30 +0300

    Add reviews fetching and displaying

---
+++ main.js
@@ -0,0 +1,22 @@
+const requestData = async (url, templateSelector = '') => {
+    const templateElem = document.querySelector(templateSelector)
+    const data = await (await fetch(url)).json()
+
+    data.forEach(obj => {
+        let element = templateElem.content.cloneNode(true)
+        const fields = Object.keys(obj)
+        fields.forEach(
+            field => {
+                let elem = element.querySelector(`#${field}`)
+                if (elem) elem.textContent = obj[field]
+            }
+        )
+        templateElem.after(element)
+    })
+}
+
+document.addEventListener(
+    'DOMContentLoaded', () => requestData(
+        '/.netlify/functions/reviews', 'template#review'
+    )
+)
